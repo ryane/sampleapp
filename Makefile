841422042e06cec35800b0f54e9d40be74aefd67
ryane <ryanesc@gmail.com>
2019-11-14 09:39:50 -0500

    docker build and config

---
+++ Makefile
@@ -0,0 +1,12 @@
+GIT_COMMIT = $(shell git rev-parse HEAD)
+GIT_SHA    = $(shell git rev-parse --short HEAD)
+GIT_TAG    = $(shell git describe --tags --abbrev=0 --exact-match 2>/dev/null)
+GIT_DIRTY  = $(shell test -n "`git status --porcelain`" && echo "dirty" || echo "clean")
+IMAGE_REPO = ryane
+
+.PHONY: docker
+docker:
+	docker build --force-rm -t ${IMAGE_REPO}/sampleapp:${GIT_SHA} -f ./Dockerfile .
+
+docker.push: docker
+	docker push ${IMAGE_REPO}/sampleapp:${GIT_SHA}
--- main.go
+++ main.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 )
 
-const version string = "1.0"
+const (
+	messageConfigPath string = "/config/data"
+	version           string = "1.0"
+)
+
+type response struct {
+	Message string `json:"message"`
+}
 
 func root(w http.ResponseWriter, r *http.Request) {
-	_, _ = fmt.Fprintf(w, "sampleapp version %s", version)
+	resp := &response{getMessage()}
+	data, err := json.Marshal(resp)
+	if err != nil {
+		_, _ = fmt.Fprintf(w, "error: ", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	_, _ = fmt.Fprint(w, string(data))
 }
 
 func health(w http.ResponseWriter, r *http.Request) {
@@ -18,6 +35,19 @@ func health(w http.ResponseWriter, r *http.Request) {
 
 func getVersion(w http.ResponseWriter, r *http.Request) {
 	_, _ = fmt.Fprintf(w, "%s\n", version)
+}
+
+func getMessage() string {
+	defaultMsg := "Hello"
+	if _, err := os.Stat(messageConfigPath); err == nil {
+		data, err := ioutil.ReadFile(messageConfigPath)
+		if err != nil {
+			log.Printf("error reading message config: %s", err)
+			return defaultMsg
+		}
+		return string(data)
+	}
+	return defaultMsg
 }
 
 func main() {
